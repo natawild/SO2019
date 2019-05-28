8a18db0bcffa8e21b0a845a80f48bca97fa03a06
Célia Figueiredo <celianlfg@hotmail.com>
2019-05-28 21:32:20 +0100

    update

--- ResolucaoTestes/teste26maio2017/filtroExiste.c
+++ ResolucaoTestes/teste26maio2017/filtroExiste.c
@@ -10,8 +10,8 @@
 
 /*
 Pretende-se fazer uma pesquisa concorrente para determinar a exitencia de determinado padrão 
-em linhas de texto, recebidas no stdin. Assuma a existencia de dois programas 'flitro' e 'existe' que 
-enacadeados, permitem fazer a pesquisa. O primeiro descarta oas casos mais óbvios que lhe chegam ao seu stdin, deixando passar os 
+em linhas de texto, recebidas no stdin. Assuma a existencia de dois programas 'filtro' e 'existe' que 
+enacadeados, permitem fazer a pesquisa. O primeiro descarta os casos mais óbvios que lhe chegam ao seu stdin, deixando passar os 
 restantes para o seu stout. O segundo termina com o valor de exit 1 caso receba uma linha com o padrão no seu stdin, ou com 0, caso isso 
 não aconteça, até EOF. Escreva um programa que receba um número n como argumento, e faça a pesquisa concorrente utilizando n pares de processos
 a executar 'filtro' e 'existe'. O programa deverá imprimir o resultado, logo que possivel, e terminar os processos que já não estejam a fazer
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[]) {
   char buffer[1024];
   int fd[n][2];
   int fd2[n][2];
-  int pid[n];
+  int pid[n], pid2[n];
 
   for(int i = 0; i<n; i++){
     pipe(fd[i]);
@@ -46,12 +46,12 @@ int main(int argc, char const *argv[]) {
       close(fd[i][1]);
       close(fd2[i][0]);
       execlp("filtro", "filtro", NULL);
-      _exit(1);
+      _exit(0);
     }
     close(fd[i][0]);
     close(fd2[i][1]);
 
-    if(!fork()){
+    if((pid2[i] = fork()) ==0){
       dup2(fd2[i][0], 0);
       close(fd2[i][1]);
       execlp("existe", "existe", NULL);
@@ -60,23 +60,22 @@ int main(int argc, char const *argv[]) {
     close(fd2[i][0]);
   }
   
-    if(!fork()){
-      int d = 0;
-      while((r = readln(0, buffer, 1024))){
-        write(fd[d][1], buffer, r);
-        d = (d+1)%n;
+  if(!fork()){ // tem de ser um filho a ler do stdin e a escrever pro pipe para o pai podeser seguir e esperar pela morte dos filhos
+    int d = 0;
+    while((r = readln(0, buffer, 1024))){
+      write(fd[d][1], buffer, r);
+      d = (d+1)%n;
+    }
+  }
+
+  wait(&status);
+  if(WIFEXITED(status))
+    if(WEXITSTATUS(status) == 1){
+      for(int i = 0; i<n; i++){
+        kill(pid[i], SIGKILL);
+        kill(pid2[i], SIGKILL);
       }
     }
 
-  for(int i = 0; i<n; i++){
-    wait(&status);
-    if(WIFEXITED(status))
-      if(WEXITSTATUS(status) == 1){
-        for(int i = 0; i<n; i++){
-          kill(pid[i], SIGKILL);
-          _exit(0);
-        }
-      }
-  }
   return 0;
 }
